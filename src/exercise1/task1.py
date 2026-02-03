dcdf463d396b5cf1df455ca4efda68152a690e73
Your Name <you@example.com>
2026-02-03 20:04:16 +0300

    add task1

---
+++ src/exercise1/task1.py
@@ -0,0 +1,12 @@
+x1 = float(input("Введите значение x1: "))
+y1 = float(input("Введите значение y1: "))
+z1 = int(input("Введите значение z1: "))
+x2 = int(input("Введите значение x2: "))
+y2 = int(input("Введите значение y2: "))
+z2 = int(input("Введите значение z2: "))
+
+print(type(x1))
+
+result = (x1 * x2) + (y1 * y2) + (z1 * z2)
+
+print(result)
