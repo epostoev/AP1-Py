0983d50113fc810eddfa41581e1f35e06bc2d730
Your Name <you@example.com>
2026-02-05 19:45:02 +0300

    modified:   task1.py

--- src/exercise1/task1.py
+++ src/exercise1/task1.py
@@ -1,25 +1,28 @@
-x1 = input()
-x2 = input()
+def scalar():
+    x1 = input()
+    x2 = input()
 
-x1_split = x1.split()
-x2_split = x2.split()
+    x1_split = x1.split()
+    x2_split = x2.split()
 
-x1_split_float = []
-x2_split_float = []
+    x1_split_float = []
+    x2_split_float = []
 
-x1_split_count = len(x1_split)
-x2_split_count = len(x2_split)
+    x1_split_count = len(x1_split)
+    x2_split_count = len(x2_split)
 
-result = 0
+    result = 0
 
-for n in range(x1_split_count):
-    x1 = float(x1_split[n])
-    x2 = float(x2_split[n])
-    x1_split_float.append(x1)
-    x2_split_float.append(x2)
-    result = x1_split_float[n] * x2_split_float[n] + result
+    for n in range(x1_split_count):
+        x1 = float(x1_split[n])
+        x2 = float(x2_split[n])
+        x1_split_float.append(x1)
+        x2_split_float.append(x2)
+        result = x1_split_float[n] * x2_split_float[n] + result
 
-print(result)
+    print(result)
 
+if __name__ == "__main__":
+    scalar()
 
 # 1.0 2.0 3.0 4.0 5.0 6.0
