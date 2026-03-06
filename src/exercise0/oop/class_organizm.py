4111039dfc7d8a94c3a429e0ed86117821c708e2
Your Name <you@example.com>
2026-03-06 12:36:24 +0300

    classs_organizm

--- src/exercise0/oop/class_organizm.py
+++ src/exercise0/oop/class_organizm.py
@@ -1,32 +1,25 @@
-# ... создай три класса: ElectronicDevice, Television и Computer
-class ElectronicDevice:
-    def power_on(self):
-        print(f"Устройство включено")
-    def power_off(self):
-        print(f"Устройство выключено")
+# суперкласс
+class StringInstrument:
+    def __init__(self, name, number_of_strings):
+        self.name = name
+        self.number_of_strings = number_of_strings
 
-class Television(ElectronicDevice):
-    def change_channel(self):
-        print(f"Канал был изменен")
-class Computer(ElectronicDevice):
-    def open_application(self):
-        print(f"Приложение было открыто")
+    def play(self):
+        return f"Играем на инструменте {self.name}"
 
+    def tune(self):
+        return f"Настраиваем инструмент {self.name}. Количество струн: {self.number_of_strings}"
 
-electronic_device = ElectronicDevice()
-# ... вызови методы power_on() и power_off() для объекта electronic_device
-electronic_device.power_off()
-electronic_device.power_on()
+# подкласс Guitar
+class Guitar(StringInstrument):               
+    def __init__(self, number_of_strings):
+        # self.name = 'Гитара'
+        StringInstrument.__init__(self, 'Гитарка', number_of_strings) #вызов конструктора
+ 
+    def play_chords(self):
+        return f"Играем аккорды на инструменте {self.name}" 
 
-television = Television()
-# ... вызови методы power_on(), power_off() и change_channel() для объекта television
-television.power_on()
-television.power_off()
-television.change_channel()
+ 
+gitar_1 = Guitar(3)
 
-computer = Computer()
-# ... вызови методы power_on(), power_off() и open_application() для объекта computer
-computer.power_on()
-computer.power_off()
-computer.open_application()
-##############################################
+print(gitar_1.play_chords())
